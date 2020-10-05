d6d6992a968bbcd6700a165ab74274633eb11435
dt <dhtxhg@gmail.com>
2020-10-05 01:04:16 -0700

    local phantom material

--- project/objects/hero/Phantom.gd
+++ project/objects/hero/Phantom.gd
@@ -17,6 +17,7 @@ func _ready():
 	var game = get_tree().get_nodes_in_group("gamecontroller")[0]
 	game.connect("rewind", self, "_on_player_rewind")
 	mat = $"character/Armature/Bone/Bone001/Bone002/Bone003/Bone004/Head001".get_surface_material(0)
+	mat.resource_local_to_scene = true
 
 func _set_playback_data(_playback_data):
 	playback_data = _playback_data
@@ -24,9 +25,10 @@ func _set_playback_data(_playback_data):
 
 func _process(delta):
 	if killcomplete:
-		mat.params_grow_amount = mat.params_grow_amount + 0.08
+		mat.params_grow_amount = min(mat.params_grow_amount + 0.3,16.0)
+		mat.albedo_color = Color(mat.albedo_color.r,mat.albedo_color.g,mat.albedo_color.b,mat.albedo_color.a-0.01)
 	else:
-		mat.params_grow_amount = max(mat.params_grow_amount - 0.5,0.0)
+		mat.params_grow_amount = max(mat.params_grow_amount - 1.0,0.0)
 	
 func _process_movement(delta : float):
 	
@@ -56,6 +58,7 @@ func _animate_slash():
 	yield($Timer, "timeout")
 	
 	killcomplete = true
+	mat.albedo_color = Color(mat.albedo_color.r,mat.albedo_color.g,mat.albedo_color.b,0.5)
 	
 	target_enemy.kill()
 	$Timer.start(0.7)
@@ -80,6 +83,7 @@ func start_playback():
 func rewind(successful=true):
 	#TODO: This will be painful
 	killcomplete = false
+	mat.albedo_color = Color(mat.albedo_color.r,mat.albedo_color.g,mat.albedo_color.b,1.0)
 	
 	#Reset back to the start position
 	transform.origin = start_position
